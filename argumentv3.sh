4de314fce32f36fb42d0266e211cc18eafe815d3
Raúl San Martín Escobar <45212022+R3sM3@users.noreply.github.com>
2023-06-05 21:06:05 -0400

    Update argumentv3.sh

--- argumentv3.sh
+++ argumentv3.sh
@@ -2,7 +2,6 @@
 
 #  Este codigo fue extraido desde la siguiente pagina
 #  https://aakibkhan1.medium.com/mastering-advanced-shell-scripting-unleashing-the-full-potential-425e1757f27f
-#  Fue creado por Aakib
 
 # Nested if-else
 if [ condition1 ]; then
