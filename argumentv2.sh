a6cb96e1c08d7808ce279a0bd76551d9aac52a4b
Raúl San Martín Escobar <45212022+R3sM3@users.noreply.github.com>
2023-06-23 23:24:01 -0400

    Update argumentv2.sh

--- argumentv2.sh
+++ argumentv2.sh
@@ -3,7 +3,6 @@
 #  Este codigo fue extraido desde la siguiente pagina
 #  https://aakibkhan1.medium.com/mastering-advanced-shell-scripting-unleashing-the-full-potential-425e1757f27f
 
-
 while getopts "a:b:c" opt; do
   case $opt in
     a)
